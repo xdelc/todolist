55ef9cd42758856ab1426310970915520958bb58
xdelc <xdelc002@fiu.edu>
2020-06-06 18:47:22 -0400

    Adding .gitignore, Procfile, and updating ports

--- app.js
+++ app.js
@@ -88,6 +88,11 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+	port = 3000;
+}
+
 app.post("/", function(req, res){
   const itemName = req.body.newItem;
 	const listName = req.body.list;
@@ -129,6 +134,6 @@ app.post("/delete", function(req, res) {
 })
 
 
-app.listen(3000, function() {
-	console.log("Server started on port 3000");
+app.listen(port, function() {
+	console.log("Server has started successfully");
 });
